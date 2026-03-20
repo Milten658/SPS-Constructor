f89a99e23a05a9232b7071656ec9fa83651d5ee7
egoraptor <egoraptor04@gmail.com>
2026-03-20 21:04:03 +0200

    Ligma

--- assets/scripts/visual_script.js
+++ assets/scripts/visual_script.js
@@ -1,56 +1,46 @@
 const options = document.querySelectorAll('.posluga_wrapper');
 const body = document.body;
 
+// ===== DROPDOWN =====
+const main = document.querySelector('.dropdown_main');
+const list = document.querySelector('.dropdown_list');
+const fill = document.querySelector('.dropdown_fill');
+
+main.addEventListener('click', (e) => {
+  e.stopPropagation();
+  list.classList.toggle('active');
+});
+
+list.addEventListener('click', (e) => {
+  if (e.target.classList.contains('dropdown_option')) {
+    fill.textContent = e.target.textContent;
+    list.classList.remove('active');
+  }
+});
+
+// ===== ACCORDION =====
 options.forEach(option => {
   option.addEventListener('click', () => {
 
-    const form = option.nextElementSibling;
     const isOpen = option.classList.contains('active');
 
     options.forEach(o => o.classList.remove('active'));
-    document.querySelectorAll('.posluga_form').forEach(f => {
-      f.style.height = '0px';
-    });
 
     body.classList.remove('remont', 'obmin');
 
     if (!isOpen) {
       option.classList.add('active');
-      form.style.height = form.scrollHeight + 'px';
 
       const name = option.dataset.name;
 
       if (name) {
         body.classList.add(name);
-        function updateLittleP(name) {
-  littlePs.forEach(el => {
-    const text = el.dataset[name];
-    if (text) {
-      el.textContent = text;
-    }
-  });
-}
       }
     }
   });
 });
 
-const warrantyCheckbox = document.getElementById('warranty_checkbox');
-const warrantyFields = document.querySelectorAll('.warranty_fields select');
-
-const nonWarrantyCheckbox = document.getElementById('nonwarranty_checkbox');
-const nonWarrantyFields = document.querySelectorAll('.nonwarranty_fields select');
-
-function toggleFields(checkbox, fields) {
-  fields.forEach(field => {
-    field.disabled = !checkbox.checked;
-  });
-}
-
-warrantyCheckbox.addEventListener('change', () => {
-  toggleFields(warrantyCheckbox, warrantyFields);
-});
-
-nonWarrantyCheckbox.addEventListener('change', () => {
-  toggleFields(nonWarrantyCheckbox, nonWarrantyFields);
+// ===== CLOSE DROPDOWN =====
+document.addEventListener('click', () => {
+  list.classList.remove('active');
 });
