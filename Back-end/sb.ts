78db9148983a466a5be4aa8f1935525dbec53a70
Milten658 <ssya20031127@gmail.com>
2026-04-03 16:56:05 +0300

    Connecting to SupaBase, showing the list of devices in the searchbar + adding search function, by Model Name, by articule and by Department

---
+++ Back-end/sb.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+
+export const supabase = createClient(
+"https://jgyrkytxpdrozrfygpvl.supabase.co",
+"sb_publishable_MgpctTKQmVsqEodIggy_zw_G23nF_Pb"
+);
